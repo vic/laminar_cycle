e9520b49461a6b50290f101ada6ef0d6eade4852
Victor Hugo Borja <vborja@apache.org>
2020-05-23 00:47:02 -0500

    Generic driverPlus

--- cycle/src/package.scala
+++ cycle/src/package.scala
@@ -1,15 +1,17 @@
 package object cycle extends cycle.core.API {
   import com.raquo.laminar.api.L._
 
-//  implicit class DriverPlus[A, R](private val aDriver: DriverFn[A, R])
-//      extends AnyVal {
-//    def ++[B](
-//        bDriver: DriverFn[B, R]
-//    ): DriverFn[(A, B), R] = {
-//      val abDevices = (aDriver.devices, bDriver.devices)
-//      val abBinds   = aDriver.binds ++ bDriver.binds
-//      DriverFn[(A, B), R](abDevices, abBinds: _*)
-//    }
-//  }
+  implicit class DriverPlus[A, R](private val aDriver: DriverFn[A, R])
+      extends AnyVal {
+    def ++[B](
+        bDriver: DriverFn[B, R]
+    )(
+        implicit DriverFn: ((A, B), Binds) => DriverFn[(A, B), R]
+    ): DriverFn[(A, B), R] = {
+      val abDevices = (aDriver.devices, bDriver.devices)
+      val abBinds   = aDriver.binds ++ bDriver.binds
+      DriverFn(abDevices, abBinds)
+    }
+  }
 
 }
